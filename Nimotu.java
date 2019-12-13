771c8d787172d9a36d6ce3e70e3468fff71d2d13
Shota Kambe <8408795@gmail.com>
2019-12-13 17:47:49 +0900

    Update Nimotu.java

--- Nimotu.java
+++ Nimotu.java
@@ -6,147 +6,158 @@ public class Nimotu{
 
     public static void main(String[] args){
 
-        String[][] field = {
-            {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
-        
-        };    //field[y][x]
+        int cnt = 0;
+        String operat;
 
-        String[][] compari = {
-            {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
-            ,{"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
-        
-        };    //field[y][x]
-        
-        Random rnd = new Random();
-        int xnd =  rnd.nextInt(13) + 1;
-        int ynd = rnd.nextInt(7) + 1;
+        while(cnt < 4){
 
-        for(int i = 0; i < field.length; i++){
-            field[rnd.nextInt(7) + 1][rnd.nextInt(13) + 1]  = ".";
-        }
+            String[][] field = {
+                {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
+            
+            };    //field[y][x]
 
-        for(int i = 0; i < field.length; i++){
-            int yblock = rnd.nextInt(7) + 1;
-            int xblock = rnd.nextInt(13) + 1;
-            field[yblock][xblock]  = "#";
-            compari[yblock][xblock]  = "#";
-        }
-                
-        int xpl = xnd;
-        int ypl = ynd;
+            String[][] compari = {
+                {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"}
+                ,{"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"}
+            
+            };    //field[y][x]
 
-        field[ypl][xpl] = "p";
-        compari[ypl][xpl] = "p";
+            
+            Random rnd = new Random();
+            int xnd =  rnd.nextInt(13) + 1;
+            int ynd = rnd.nextInt(7) + 1;
 
-        Scanner scn = new Scanner(System.in);
-        end: while(true){
-            //String operat = " ";  delete later
             for(int i = 0; i < field.length; i++){
-                for(int j = 0; j < field[i].length; j ++){
-                    System.out.print(field[i][j]);
-                }
-                System.out.println();
-
+                field[rnd.nextInt(7) + 1][rnd.nextInt(13) + 1]  = ".";
             }
 
             for(int i = 0; i < field.length; i++){
-                for(int j = 0; j < field[i].length; j ++){
-                    System.out.print(compari[i][j]);
+                int yblock = rnd.nextInt(7) + 1;
+                int xblock = rnd.nextInt(13) + 1;
+                field[yblock][xblock]  = "#";
+                compari[yblock][xblock]  = "#";
+            }
+                    
+            int xpl = xnd;
+            int ypl = ynd;
+
+            field[ypl][xpl] = "p";
+            compari[ypl][xpl] = "p";
+
+            Scanner scn = new Scanner(System.in);
+            end: while(true){
+                //String operat = " ";  delete later
+                for(int i = 0; i < field.length; i++){
+                    for(int j = 0; j < field[i].length; j ++){
+                        System.out.print(field[i][j]);
+                    }
+                    System.out.println();
+
                 }
-                System.out.println();
 
-            }
+                for(int i = 0; i < field.length; i++){
+                    for(int j = 0; j < field[i].length; j ++){
+                        System.out.print(compari[i][j]);
+                    }
+                    System.out.println();
 
-            if(Arrays.deepEquals(field, compari)){
-                System.out.println("Congrats!");
-                break end;
-            }
+                }
 
-            System.out.println("input \n w: ↑ \n d: →\n s: ↓ \n a: ← ");
-            String operat = scn.nextLine();
+                if(Arrays.deepEquals(field, compari)){
+                    System.out.println("NEXT!");
+                    break end;
+                }
 
-            String tmp;        
+                System.out.println("input \n w: ↑ \n d: →\n s: ↓ \n a: ← ");
+                operat   = scn.nextLine();
+
+                String tmp;        
+                    
+                switch(operat){
+                    case "w":
+                        if(field[ypl-1][xpl].equals("#")){
+                            break;
+                        }else if(field[ypl-1][xpl].equals(".")){
+                            field[ypl-1][xpl] = " ";
+                        }
+                        tmp = field[ypl-1][xpl];
+                        field[ypl-1][xpl]  = field[ypl][xpl];
+                        field[ypl][xpl] = tmp;
+                        tmp = compari[ypl-1][xpl];
+                        compari[ypl-1][xpl]  = compari[ypl][xpl];
+                        compari[ypl][xpl] = tmp;
+                        ypl = ypl-1;
+                        break;
+                    case "s":
+                        if(field[ypl+1][xpl].equals("#")){
+                            break;
+                        }else if(field[ypl+1][xpl].equals(".")){
+                            field[ypl+1][xpl] = " ";
+                        }
+                        tmp = field[ypl+1][xpl];
+                        field[ypl+1][xpl]  = field[ypl][xpl];
+                        field[ypl][xpl] = tmp;
+                        tmp = compari[ypl+1][xpl];
+                        compari[ypl+1][xpl]  = compari[ypl][xpl];
+                        compari[ypl][xpl] = tmp;
+                        ypl = ypl+1;
+                        break;
+                    case "d":
+                        if(field[ypl][xpl+1].equals("#")){
+                            break;
+                        }else if(field[ypl][xpl+1].equals(".")){
+                            field[ypl][xpl+1] = " ";
+                        }
+                        tmp = field[ypl][xpl+1];
+                        field[ypl][xpl+1]  = field[ypl][xpl];
+                        field[ypl][xpl] = tmp;
+                        tmp = compari[ypl][xpl+1];
+                        compari[ypl][xpl+1]  = compari[ypl][xpl];
+                        compari[ypl][xpl] = tmp;
+                        xpl = xpl + 1;
+                        break;
+                    case "a":
+                        if(field[ypl][xpl-1].equals("#")){
+                            break;
+                        }else if(field[ypl][xpl-1].equals(".")){
+                            field[ypl][xpl-1] = " ";
+                        }
+                        tmp = field[ypl][xpl-1];
+                        field[ypl][xpl-1]  = field[ypl][xpl];
+                        field[ypl][xpl] = tmp;
+                        tmp = compari[ypl][xpl-1];
+                        compari[ypl][xpl-1]  = compari[ypl][xpl];
+                        compari[ypl][xpl] = tmp;
+                        xpl = xpl - 1;
+                        break;
+                }
                 
-            switch(operat){
-                case "w":
-                    if(field[ypl-1][xpl].equals("#")){
-                        break;
-                    }else if(field[ypl-1][xpl].equals(".")){
-                        field[ypl-1][xpl] = " ";
-                    }
-                    tmp = field[ypl-1][xpl];
-                    field[ypl-1][xpl]  = field[ypl][xpl];
-                    field[ypl][xpl] = tmp;
-                    tmp = compari[ypl-1][xpl];
-                    compari[ypl-1][xpl]  = compari[ypl][xpl];
-                    compari[ypl][xpl] = tmp;
-                    ypl = ypl-1;
-                    break;
-                case "s":
-                    if(field[ypl+1][xpl].equals("#")){
-                        break;
-                    }else if(field[ypl+1][xpl].equals(".")){
-                        field[ypl+1][xpl] = " ";
-                    }
-                    tmp = field[ypl+1][xpl];
-                    field[ypl+1][xpl]  = field[ypl][xpl];
-                    field[ypl][xpl] = tmp;
-                    tmp = compari[ypl+1][xpl];
-                    compari[ypl+1][xpl]  = compari[ypl][xpl];
-                    compari[ypl][xpl] = tmp;
-                    ypl = ypl+1;
-                    break;
-                case "d":
-                    if(field[ypl][xpl+1].equals("#")){
-                        break;
-                    }else if(field[ypl][xpl+1].equals(".")){
-                        field[ypl][xpl+1] = " ";
-                    }
-                    tmp = field[ypl][xpl+1];
-                    field[ypl][xpl+1]  = field[ypl][xpl];
-                    field[ypl][xpl] = tmp;
-                    tmp = compari[ypl][xpl+1];
-                    compari[ypl][xpl+1]  = compari[ypl][xpl];
-                    compari[ypl][xpl] = tmp;
-                    xpl = xpl + 1;
-                    break;
-                case "a":
-                    if(field[ypl][xpl-1].equals("#")){
-                        break;
-                    }else if(field[ypl][xpl-1].equals(".")){
-                        field[ypl][xpl-1] = " ";
-                    }
-                    tmp = field[ypl][xpl-1];
-                    field[ypl][xpl-1]  = field[ypl][xpl];
-                    field[ypl][xpl] = tmp;
-                    tmp = compari[ypl][xpl-1];
-                    compari[ypl][xpl-1]  = compari[ypl][xpl];
-                    compari[ypl][xpl] = tmp;
-                    xpl = xpl - 1;
-                    break;
-            }
-            
 
-            
+                
+        
+            }
+            scn.close();
+            cnt++;
+                
         }
-        scn.close();
+        
     }
 }
 
